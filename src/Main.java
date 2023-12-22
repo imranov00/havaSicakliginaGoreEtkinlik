4f9a021d739a83dd6c676818fd7695be83fbd4de
İmranov <semtexpert@gmail.com>
2023-12-22 14:56:29 +0300

    Etkinlik Önerisi Oluşturuldu

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Hava Sıcaklığını Giriniz : ");
+        heat = input.nextInt();
+
+        if (heat <= 5){
+            System.out.print("Kayak Yapabilirsiniz.");
+        } else if (heat <= 25){
+            if (heat >=5 && heat <= 15){
+                System.out.println("Sinemaya Gidebilirsiniz. ");
+            }if (heat >= 15 && heat <=25 ){
+                System.out.println("Pikniğe Gidebilirsiniz. ");
+            }
+        } else if (heat >=25) {
+            System.out.println("Yüzmeye Gidebilirsiniz. ");
+        }
+    }
+}
